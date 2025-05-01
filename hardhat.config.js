f4f342e91ecf7f901a91bcdfc771bb0777fd8db3
Vishesh <visheshvasu2305@gmail.com>
2025-05-01 20:52:16 +0530

    CHanges made

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.19", // Or your Solidity version
+  networks: {
+    ganache: {
+      url: "http://localhost:7545", // Ganache RPC server address
+      accounts: {
+        mnemonic: "test test test test test test test test test test test test", // Or the one Ganache provides
+        count: 10,
+      },
+    },
+  },
 };
